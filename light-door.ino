37ad628abb885797e85b585729f4954771fb6750
gabylan <you@example.com>
2020-01-12 13:04:37 -0300

    add diagram fritzing

--- light-door.ino
+++ light-door.ino
@@ -1,30 +1,36 @@
+
+#include <AsyncDelay.h>
 /*
  * STATUS
- *                      30 seconds without pirStatus = HIGH
- *   WITHOUT_ACTIVITY <------------------  
- *         |                             | 
- *         | pirStatus=HIGH &&           |          
- *         | lightSensor >= LIGHT_LEVEL  |   
- *          --------------------> ACTIVITY_DETECTED 
- *                                           
+ *                 (evaluate each 10 seconds)
+ *                 lightSensor < LIGHT_LEVEL
+ *                   ----------------->  
+ *   NATURAL_LIGHT                        DARKNESS (light off)
+      (light off)   <------------------             
+                  lightSensor >= LIGHT_LEVEL    |   | 
+ *                                              |   | 
+ *                                     timeout^ |   |  pirStatus=HIGH        
+ *                                              |   |   
+ *                                      ACTIVITY (for 45 seconds, if pirStatus=HIGH reset timeout)
+ *                                        (light on)   
  */
 
 
 /** CONSTANTS */
 const int RELAY = 2;
-const int SENSOR_LIGHT = A0;
-const int LIGHT_LEVEL = 500;
-const long INTERVAL = 60000;
+const int PIR = 3;
+const int PHOTOCELL = A0;
+const int LIGHT_LEVEL = 30;
+const long INTERVAL = 45000;
 
 
 /** state machine */
-
-const int WITHOUT_ACTIVITY = 0;
-const int ACTIVITY_DETECTED = 1; 
-const int DOOR_CLOSED = 2;
-const int DOOR_CLOSED_ACTIVITY = 3;
-int state = WITHOUT_ACTIVITY;
+const int NATURAL_LIGHT = 0;
+const int DARKNESS = 1; 
+const int ACTIVITY = 2;
+int state = DARKNESS;
 unsigned long currentMillis;
+AsyncDelay readPhotocell;
 
 
 
@@ -33,43 +39,66 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RELAY, OUTPUT);
-  pinMode(SENSOR_LIGHT, INPUT);
+  pinMode(PHOTOCELL, INPUT);
+  pinMode(PIR, INPUT);
   digitalWrite(RELAY, LOW); 
   digitalWrite(LED_BUILTIN, LOW);   
+  
+  //PHOTOCELL
+  readPhotocell.start(10000, AsyncDelay::MILLIS);
   Serial.begin(9600);
+  Serial.println("START");
 }
 
 // the loop function runs over and over again forever
 void loop() {
 
-  int lightSensor = analogRead(SENSOR_LIGHT );
-  switch(state){
-    case WITHOUT_ACTIVITY:{
-      //if (pirStatus == HIGH) {
-      if (lightSensor >= LIGHT_LEVEL) {
-        Serial.println("ACTIVITY_DETECTED");
-        state = ACTIVITY_DETECTED;
-        currentMillis = millis();
-        digitalWrite(LED_BUILTIN, HIGH); // TURN ON LIGHT
-        digitalWrite(RELAY, HIGH);  
-        delay(3000);
-      } 
+
+  //process photocell
+  if (readPhotocell.isExpired() && state != ACTIVITY ) {
+    int photocellReading  = map(analogRead(PHOTOCELL), 0, 1023, 0, 100);
+    Serial.println(photocellReading);
+    if(photocellReading > LIGHT_LEVEL){
+      Serial.println("NATURAL_LIGHT");
+      state = NATURAL_LIGHT;
     }
-    break;
-    
-    case ACTIVITY_DETECTED:{
-//      if(pirStatus == HIGH){
-      if(false){
-        currentMillis = millis();
-      }
-      else if(millis() > currentMillis + INTERVAL){
-        Serial.println("WITHOUT_ACTIVITY");
-        state = WITHOUT_ACTIVITY;
-        digitalWrite(LED_BUILTIN, LOW); // TURN OFF LIGHT
-        digitalWrite(RELAY, LOW);  
-      }
+    else{
+      Serial.println("DARKNESS");
+      state = DARKNESS;
     }
-    break;
+    readPhotocell.repeat();
+  }
+
+  //process pir
+  if(state !=NATURAL_LIGHT){
+    int pirStatus = digitalRead(PIR);
+    switch(state){
+      case DARKNESS:{
+        if (pirStatus == HIGH) {
+          currentMillis = millis();
+          Serial.println("ACTIVITY");
+          state = ACTIVITY;
+          digitalWrite(LED_BUILTIN, HIGH); // TURN ON LIGHT
+          digitalWrite(RELAY, HIGH);  
+          delay(3000);
+        }
+      }
+      break;
+      
+      case ACTIVITY:{
+        if(pirStatus == HIGH){
+          currentMillis = millis();
+          Serial.println("reset");
+        }
+        else if(millis() > currentMillis + INTERVAL){
+          Serial.println("DARKNESS");
+          state = DARKNESS;
+          digitalWrite(LED_BUILTIN, LOW); // TURN OFF LIGHT
+          digitalWrite(RELAY, LOW);  
+        }
+      }
+      break;
+    }
   }
     
    
